287569630c0b4327148488fea114dee0b5ac615a
hewanbing <hewanbing@fudan.edu.cn>
2022-07-12 17:23:37 +0800

    first commit

--- startNginx.sh
+++ startNginx.sh
@@ -1,2 +1,2 @@
-sudo singularity instance start --writable-tmpfs -B .:/var/www/html  nginx.sif nginx
-
+sudo singularity instance start --writable-tmpfs -B ./:/usr/share/nginx/html nginx.sif nginx
+#sudo singularity exec --writable-tmpfs -B ./:/usr/share/nginx/html nginx.sif nginx
